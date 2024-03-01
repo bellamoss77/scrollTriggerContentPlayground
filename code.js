ace9dc6dffe7eb964943d62e449d646a62225e08
Maggie <mmf295@icloud.com>
2024-03-01 01:47:43 -0500

    work on lightbox PDF reader

--- code.js
+++ code.js
@@ -69,7 +69,7 @@ gsap.from('.fade-in', {
     },
     opacity: 0,
     duration: 2
-})
+});
 
 gsap.from('.right-slide-in', {
     scrollTrigger: {
@@ -82,4 +82,73 @@ gsap.from('.right-slide-in', {
     opacity: 0,
     ease: 'bounce',
     duration: 2
-})
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const lightbox = document.getElementById('lightbox');
+    const pdfRenderContainer = document.getElementById('pdf-render-container');
+    const closeLightboxBtn = document.getElementById('close');
+
+    function openLightbox(pdfURL) {
+        gsap.to(lightbox, { autoAlpha: 1, duration: 0.5 });
+        lightbox.style.display = 'flex';
+        lightbox.style.opacity = '1';
+        lightbox.style.visibility = 'visible';
+        renderPDF(pdfURL, pdfRenderContainer);
+    }
+
+    function closeLightbox() {
+        gsap.to(lightbox, { autoAlpha: 0, duration: 0.5, onComplete: () => {
+            lightbox.style.display = 'none';
+            lightbox.style.opacity = '0';
+            lightbox.style.opacity = 'hidden'
+            pdfRenderContainer.innerHTML = ''; 
+        }});
+    }
+
+    //pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+
+    document.querySelectorAll('.gallery-img').forEach(item => {
+        item.addEventListener('click', function() {
+            const pdfURL = this.getAttribute('data-pdf');
+            openLightbox(pdfURL);
+        });
+    });
+
+    closeLightboxBtn.addEventListener('click', closeLightbox);
+
+    lightbox.addEventListener('click', function(e) {
+        if (e.target === lightbox) closeLightbox();
+    });
+
+    gsap.set(lightbox, { autoAlpha: 0 });
+
+    function renderPDF(pdfURL, container) {
+        var loadingTask = pdfjsLib.getDocument(pdfURL);
+        loadingTask.promise.then(pdf => {
+            console.log('PDF loaded');
+            
+            pdf.getPage(1).then(page => {
+                console.log('Page loaded');
+                
+                var viewport = page.getViewport({ scale: 1.5 });
+                var canvas = document.createElement('canvas');
+                var context = canvas.getContext('2d');
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+
+                var renderContext = {
+                    canvasContext: context,
+                    viewport: viewport
+                };
+                
+                page.render(renderContext).promise.then(() => {
+                    console.log('Page rendered');
+                    container.appendChild(canvas);
+                });
+            });
+        }, reason => console.error(reason));
+    }
+});
+  
+ 
